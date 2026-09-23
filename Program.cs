fdf19163f015a3a4ddbf4a22d204fd5289700622
bitsozs <yuwmuks@gmail.com>
2026-09-23 14:48:08 +0700

    First

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Character name: ");
+string charName = Console.ReadLine();
+Console.Write("EXP gained per battle: ");
+int exp = int.Parse(Console.ReadLine());
+
+for (int battles = 1; battles <= 3; battles++)
+{
+    Console.WriteLine($"Battle {battles} | EXP gained: {exp} | Total EXP: {exp * battles}");
+}
+Console.WriteLine();
+Console.WriteLine("=== Battle Complete ===");
+Console.WriteLine($"Characther  : {charName}\nBattles     : 3\nTotal EXP   : {exp * 3}");
