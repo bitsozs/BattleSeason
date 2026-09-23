28ada7c8ab56331b21ccf394a08191f57d7cfc0d
bitsozs <yuwmuks@gmail.com>
2026-09-23 14:51:22 +0700

    Adding Milestone

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ int exp = int.Parse(Console.ReadLine());
 for (int battles = 1; battles <= 3; battles++)
 {
     Console.WriteLine($"Battle {battles} | EXP gained: {exp} | Total EXP: {exp * battles}");
+    if (exp % 100==0)
+    {
+        Console.WriteLine($"*** Milestone reached! Total EXP: {exp}");
+    }
 }
 Console.WriteLine();
 Console.WriteLine("=== Battle Complete ===");
