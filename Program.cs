3df144950e10ed955cdcc01da816ffc7a5bb8e87
bitsozs <yuwmuks@gmail.com>
2026-09-23 15:05:33 +0700

    Fix 1

--- Program.cs
+++ Program.cs
@@ -2,15 +2,17 @@
 string charName = Console.ReadLine();
 Console.Write("EXP gained per battle: ");
 int exp = int.Parse(Console.ReadLine());
+Console.Write("Number of Battles: ");
+int totalBattles = int.Parse(Console.ReadLine());
 
-for (int battles = 1; battles <= 3; battles++)
+for (int battles = 1; battles <= totalBattles; battles++)
 {
     Console.WriteLine($"Battle {battles} | EXP gained: {exp} | Total EXP: {exp * battles}");
     if (exp % 100==0)
     {
-        Console.WriteLine($"*** Milestone reached! Total EXP: {exp}");
+        Console.WriteLine($"*** Milestone reached! Total EXP: {exp} ***");
     }
 }
 Console.WriteLine();
 Console.WriteLine("=== Battle Complete ===");
-Console.WriteLine($"Characther  : {charName}\nBattles     : 3\nTotal EXP   : {exp * 3}");
+Console.WriteLine($"Character  : {charName}\nBattles    : {totalBattles}\nTotal EXP  : {exp * totalBattles}");
